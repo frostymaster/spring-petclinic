4a8ae076eee7ad0a5b2e3ad2fb9df2be0a5bb7ac
MOSCOW\Igor_Usachev <Igor_Usachev@epam.com>
2017-04-10 14:46:01 +0300

    eqweqwewq

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -31,6 +31,7 @@ public class PetClinicApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(PetClinicApplication.class, args);
         //test
+        //tets
     }
 
 }
